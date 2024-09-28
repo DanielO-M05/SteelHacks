b638b3ae226a7660d3d6f165c95403e1762ddb53
unknown <dlo21@pitt.edu>
2024-09-28 17:47:15 -0400

    env

--- note/src/App.jsx
+++ note/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
   const [notes, setNotes] = useState([]);
 
   const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY);
+  console.log(import.meta.env.VITE_API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
   const prompt = "Write a story about a magic backpack.";
