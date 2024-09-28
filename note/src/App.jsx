748578415f97c72c1b5b80a92a0529edf1b0432c
Steve <panthalukarans@gmail.com>
2024-09-28 17:08:04 -0400

    n

--- note/src/App.jsx
+++ note/src/App.jsx
@@ -35,50 +35,81 @@ function App() {
 export default <App>*/
 
 import { useEffect, useState } from 'react';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+
+
 
 function App() {
-  const [response, setResponse] = useState('');
-  const [ws, setWs] = useState(null);
+    const [response, setResponse] = useState('');
+    const [input, setInput] = useState('');
+    const [name, setName] = useState('');
+    const [ws, setWs] = useState(null);
 
-  useEffect(() => {
-    // Create WebSocket connection
-    const socket = new WebSocket('ws://localhost:3000');
 
-    // Set WebSocket instance
-    setWs(socket);
+    useEffect(() => {
+        // Create WebSocket connection
+        const socket = new WebSocket('ws://localhost:3000');
 
-    // Listen for messages from the server
-    socket.onmessage = (event) => {
-      setResponse(event.data);
+        // Set WebSocket instance
+        setWs(socket);
+
+        // Listen for messages from the server
+        socket.onmessage = (event) => {
+            setResponse(event.data);
+        };
+
+        socket.onopen = () => {
+            console.log('Connected to the WebSocket server');
+        };
+
+        socket.onclose = () => {
+            console.log('Disconnected from the WebSocket server');
+        };
+
+        // Cleanup on component unmount
+        return () => {
+            socket.close();
+        };
+    }, []);
+
+    const sendMessage = () => {
+        if (ws) {
+            ws.send('Hello from React!');
+        }
     };
 
-    socket.onopen = () => {
-      console.log('Connected to the WebSocket server');
-    };
-
-    socket.onclose = () => {
-      console.log('Disconnected from the WebSocket server');
-    };
-
-    // Cleanup on component unmount
-    return () => {
-      socket.close();
-    };
-  }, []);
-
-  const sendMessage = () => {
-    if (ws) {
-      ws.send('Hello from React!');
+    const handleClear = () => {
+        setInput("");
     }
-  };
 
-  return (
-    <div>
-      <h1>WebSocket with Vite + React using ws</h1>
-      <button onClick={sendMessage}>Send Message to Server</button>
-      <p>Response from server: {response}</p>
-    </div>
-  );
+    const handleInput = () => {
+        console.log("yippee");
+    }
+
+    return (
+        <>
+            <div>
+                <h1>Note Sharing App</h1>
+                <input
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Name"
+                />
+                <textarea
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
+                <button onClick={handleClear}>Add Sticky</button>
+                <p>You inputted: {input}</p>
+                <p>Your name is: {name}</p>
+                <br></br>
+                <br></br>
+                <br></br>
+                {/* <button onClick={sendMessage}>Send Message to Server</button>
+        <p>Response from server: {response}</p> */}
+            </div>
+        </>
+    );
 }
 
 export default App;
