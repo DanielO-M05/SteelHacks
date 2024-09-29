108c5afd7abcdd7d749134dd76795d51d2c46c98
Steve <panthalukarans@gmail.com>
2024-09-28 21:33:59 -0400

    f

--- note/src/App.jsx
+++ note/src/App.jsx
@@ -13,8 +13,7 @@ function App() {
   const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-  const prompt = "Write a story about a magic backpack.";
-  setSummary(model.generateContent(prompt));
+    const prompt = "Can you summarize these notes: ";
 
     useEffect(() => {
         // Create WebSocket connection
